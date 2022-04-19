69d25ec9599250f076c5822edd059cd3fe6a7c16
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-04-19 22:12:39 +0000

    Save book in local storage

--- main.js
+++ main.js
@@ -4,40 +4,46 @@ const btn = document.querySelector(".btn");
 const form = document.getElementsByTagName("form")[0];
 const title = document.querySelector("#title");
 
-const array = [];
-function createBook(event) {
+let array = [];
 
+function createBook(event) {
   event.preventDefault();
   let bookTitle = title.value;
   let bookAuthor = author.value;
-    let book = { Title: bookTitle, Author: bookAuthor };
-    
+  let book = { Title: bookTitle, Author: bookAuthor };
+
+  const local = window.localStorage.getItem('book');
+  array = local ? JSON.parse(local) : [];
   array.push(book);
-   
+  localStorage.setItem('book', JSON.stringify(array));
+
+
   let bookList = document.createElement("li");
   let titlep = document.createElement("p");
   let authorP = document.createElement("p");
- let button = document.createElement("button");
-    
+  let button = document.createElement("button");
+
   button.classList.add("delete");
   button.textContent = "Remove";
-    
+
   titlep.textContent = book.Title;
   authorP.textContent = book.Author;
-    
+
   bookList.append(titlep, authorP, button);
   bookContainer.appendChild(bookList);
-  
 
-    const ul = bookContainer.children;
+  const ul = bookContainer.children;
 
-    Array.from(ul).forEach(li => {
-        const button = li.lastChild;
-        button.addEventListener('click', () => {
-            li.remove(li.childNodes);
-        });
-    });
-
+  Array.from(ul).forEach((li) => {
+      const button = li.lastChild;
+      const pos = Array.from(ul).indexOf(li);
+      console.log(pos);
+      button.addEventListener("click", () => {
+          li.remove(li.childNodes);
+          array.splice(pos, 1);
+      });
+    
+  });
 }
 
 form.addEventListener("submit", createBook);
