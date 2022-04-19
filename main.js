3e4fea8572f1be110b626bbcc34c4ca1a0a4f60f
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-19 23:17:13 +0100

    Fix linters issues

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-const bookContainer = document.querySelector(".book-container");
-const author = document.querySelector("#author");
-const btn = document.querySelector(".btn");
-const form = document.getElementsByTagName("form")[0];
-const title = document.querySelector("#title");
+const bookContainer = document.querySelector('.book-container');
+const author = document.querySelector('#author');
+const form = document.getElementsByTagName('form')[0];
+const title = document.querySelector('#title');
 
 let array = [];
 
 function createBook(event) {
   event.preventDefault();
-  let bookTitle = title.value;
-  let bookAuthor = author.value;
-  let book = { Title: bookTitle, Author: bookAuthor };
+  const bookTitle = title.value;
+  const bookAuthor = author.value;
+  const book = { Title: bookTitle, Author: bookAuthor };
 
   const local = window.localStorage.getItem('book');
   array = local ? JSON.parse(local) : [];
   array.push(book);
   localStorage.setItem('book', JSON.stringify(array));
 
+  const bookList = document.createElement('li');
+  const titlep = document.createElement('p');
+  const authorP = document.createElement('p');
+  const button = document.createElement('button');
 
-  let bookList = document.createElement("li");
-  let titlep = document.createElement("p");
-  let authorP = document.createElement("p");
-  let button = document.createElement("button");
-
-  button.classList.add("delete");
-  button.textContent = "Remove";
+  button.classList.add('delete');
+  button.textContent = 'Remove';
 
   titlep.textContent = book.Title;
   authorP.textContent = book.Author;
@@ -35,15 +33,13 @@ function createBook(event) {
   const ul = bookContainer.children;
 
   Array.from(ul).forEach((li) => {
-      const button = li.lastChild;
-      const pos = Array.from(ul).indexOf(li);
-      console.log(pos);
-      button.addEventListener("click", () => {
-          li.remove(li.childNodes);
-          array.splice(pos, 1);
-      });
-    
+    const button = li.lastChild;
+    const pos = Array.from(ul).indexOf(li);
+    button.addEventListener('click', () => {
+      li.remove(li.childNodes);
+      array.splice(pos, 1);
+    });
   });
 }
 
-form.addEventListener("submit", createBook);
+form.addEventListener('submit', createBook);
