6224cf2d99bb6e8a695a43a31e1a0720b5e5b42d
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-21 18:28:40 +0100

    Fix linters and modified the classes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 const bookContainer = document.querySelector('.book-container')
 const author = document.querySelector('#author')
 const form = document.getElementsByTagName('form')[0]
@@ -6,67 +7,74 @@ const title = document.querySelector('#title')
 // Creating a book class
 class Book {
   constructor(title, author, id = new Date().getTime()) {
-    this.title = title;
-    this.author = author;
-    this.id = id;
+    this.title = title
+    this.author = author
+    this.id = id
   }
 }
 
-let array = [];
+let array = []
 
-function createBook(book) {
-  const bookList = document.createElement('li')
-  bookList.innerHTML = `<p class="title">"${book.title}" by ${book.author}</p>
-      <button id=${book.id} class="delete" onclick= "deleteBook(event)">Remove</button>`
-  bookContainer.appendChild(bookList)
-}
-
-function deleteBook(event){
-      event.target.parentElement.remove();
-      array.splice(array.indexOf(event.target), 1)
-      localStorage.setItem('books', JSON.stringify(array));
-}
-
-function setStorage() {
-  localStorage.setItem('books', JSON.stringify(array))
-}
-
-function getStorage() {
-  if (localStorage.getItem('books') === null) {
-    setStorage()
-  } else {
-    array = JSON.parse(localStorage.getItem('books'))
+class bookLibrary {
+  static renderBooks() {
+    bookStore.getStorage()
+    array.forEach((book) => {
+      bookLibrary.createBook(book)
+    })
   }
-}
 
-function clearField() {
-  title.value = ''
-  author.value = ''
+  static createBook(book) {
+    const bookList = document.createElement('li')
+    bookList.innerHTML = `<p class="title">"${book.title}" by ${book.author}</p>
+      <button id=${book.id} class="delete" onclick= "bookLibrary.deleteBook(event)">Remove</button>`
+    bookContainer.appendChild(bookList)
+  }
+
+  static deleteBook(event) {
+    const books = bookStore.getStorage()
+    books.forEach(book=>{
+      if(event.target.id === book.id){
+        event.target.parentElement.remove()
+        books.splice(books.indexOf(book), 1)
+      }
+    })
+    localStorage.setItem('books', JSON.stringify(array))
+  }
+
+  static clearField() {
+    title.value = ''
+    author.value = ''
+  }
 }
 
 // Store data in local storage
-function store(event) {
-  const booktitle = title.value
-  const bookauthor = author.value
-  const time = new Date().getTime()
-  const id = time.toString()
-  const book = new Book(booktitle, bookauthor, id)
-  console.log(book);
-  getStorage()
-  array.push(book)
-  console.log(array);
-  setStorage()
-  createBook(book)
-  clearField()
-  event.preventDefault()
+class bookStore {
+  static setStorage() {
+    localStorage.setItem('books', JSON.stringify(array))
+  }
+
+  static getStorage() {
+    if (localStorage.getItem('books') === null) {
+      bookStore.setStorage()
+    } else {
+      return (array = JSON.parse(localStorage.getItem('books')))
+    }
+  }
+
+  static store(event) {
+    const booktitle = title.value
+    const bookauthor = author.value
+    const time = new Date().getTime()
+    const id = time.toString()
+    const book = new Book(booktitle, bookauthor, id)
+    bookStore.getStorage()
+    array.push(book)
+    bookStore.setStorage()
+    bookLibrary.createBook(book)
+    bookLibrary.clearField()
+    event.preventDefault()
+  }
 }
 
-function renderBooks() {
-  getStorage()
-  array.forEach((book) => {
-    createBook(book)
-  })
-}
-renderBooks()
-
-form.addEventListener('submit', store)
+document.addEventListener('DOMContentLoaded', bookLibrary.renderBooks)
+form.addEventListener('submit', bookStore.store)
